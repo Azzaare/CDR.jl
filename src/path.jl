fddc50410c9d23a5455d3de8d0c211da09f5e146
chiumk <cmk.kenny@gmail.com>
2018-01-15 16:07:28 +0900

    Kennytest (#4)
    * add construction in 3D
    
    * functions in 3-D

--- src/path.jl
+++ src/path.jl
@@ -1,7 +1,7 @@
-abstract type Dimension end
+#abstract type Dimension end
 
-struct Dim2 <: Dimension end
-struct Dim3 <: Dimension end
+#struct Dim2 <: Dimension end
+#struct Dim3 <: Dimension end
 
 const Sequence = Vector{Int}
 const Sequences = Vector{Sequence}
@@ -9,9 +9,9 @@ const Sequences = Vector{Sequence}
 function cdr(
     x::Int,
     y::Int,
-    sequence::Sequence,
-    dim::Dim2
+    sequence::Sequence
     )
+
     direction = ones(Int, x + y)
 
     filter_sequence = filter(a -> a < x + y, sequence)
@@ -34,11 +34,48 @@ function cdr(
     return path
 end
 
+# function cdr(
+#     x::Int,
+#     y::Int,
+#     sequences::Sequences,
+#     dim::Dimension = Dim2()
+#     )
+#     return cdr(x, y, sequences[1], dim)
+# end
+
 function cdr(
     x::Int,
     y::Int,
-    sequences::Sequences;
-    dim::Dimension = Dim2()
+    z::Int,
+    sequence::Sequence
     )
-    return cdr(x, y, sequences[1], dim)
+
+    direction = zeros(Int, x + y + z)
+    # 0 = x
+    # 1 = y
+    # 2 = z
+
+    filter_sequence = filter(a -> a < x + y + z, sequence)
+
+    for i in x+1:x+y
+        direction[filter_sequence[i] + 1] = 1
+    end
+    for i in x+y+1:x+y+z
+        direction[filter_sequence[i] + 1] = 2
+    end
+
+    path = zeros(Int, 3, x + y + z + 1)
+
+    for i in 2:(x + y + z + 1)
+        if direction[i - 1] == 0
+            move = [1, 0, 0]
+        elseif direction[i - 1] == 1
+            move = [0, 1, 0]
+        else
+            move = [0, 0, 1]
+        end
+        path[:, i] = path[:, i - 1] + move
+    end
+
+    return path
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,45 @@
 using CDR
 using Base.Test
 
+#Pkg.test("CDR")
+
 # TODO: write basic tests
 
 hausdorff_describe(10, neighbors = 2)
 hausdorff_describe(10, neighbors = 1)
 hausdorff_describe(10)
+
+dim = Dim3()
+CDR.describe(CDR.hausdorff_map(16, Dim3(),  NormalWeight()))
+CDR.describe(CDR.hausdorff_map(32, Dim3(), NormalWeight()))
+CDR.describe(CDR.hausdorff_map(64, Dim3(), NormalWeight()))
+
+CDR.describe(CDR.hausdorff_map(16, Dim3()  ))
+CDR.describe(CDR.hausdorff_map(32, Dim3() ))
+CDR.describe(CDR.hausdorff_map(64, Dim3()))
+
+hausdorff_describe(16, dim)
+hausdorff_describe(32, dim)
+hausdorff_describe(64, dim)
+
+sequence = CDR.total_order(8, CorputSequence())
+path = CDR.cdr(2,2,4, sequence)
+
+println(sequence)
+
+for i = 1:9
+  println(path[:,i])
+end
+
+println(CDR.hausdorff_distance(path, Dim3()) )
+
+w = CDR.weight(8, Log2Weight(), Dim3())
+
+println(w(2,2,4) )
+
+println( CDR.hausdorff_map(4, Dim3()) )
+CDR.describe(CDR.hausdorff_map(4, Dim3()))
+
 # hausdorff_describe(50)
 # hausdorff_describe(100)
 # hausdorff_describe(500)
